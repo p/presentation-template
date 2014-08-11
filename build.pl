b7ebce8fbd89cd3fecd3c901242ab695e1627455
Oleg Pudeyev <oleg@bsdpower.com>
2014-08-11 11:45:58 -0400

    Framework for my content

---
+++ build.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $content = `slimrb slides.slim`;
+print $!;
+
+open FH, '<boilerplate.html' or die "Can't open boilerplate: $!";
+undef $/;
+my $template = <FH>;
+close FH;
+
+$template =~ s/{{slides}}/$content/;
+
+open FH, '>index.html' or die "Can't open index: $!";
+print FH $template;
+close FH;
